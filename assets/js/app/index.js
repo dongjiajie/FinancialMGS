c349f66fee712472d10b9eda31779a0e4a1042ba
Jie <837270667@qq.com>
2017-07-11 23:02:40 +0800

    解决留言中提到的问题

--- assets/js/app/index.js
+++ assets/js/app/index.js
@@ -1,8 +1,5 @@
-/**
- * Created by dongjj on 2017/7/11.
- */
-var koa = require('koa');
-var app = new koa();
+const koa = require('koa');
+const app = new koa();
 
 app.use(function *(){
     console.log(this);
